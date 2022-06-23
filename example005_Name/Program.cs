9b738e3d99817a07eee711bea05513298cf5cbab
Olga Kosukhina <heeeelga@gmail.com>
2022-06-23 15:51:28 +0300

    add condition

---
+++ example005_Name/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Insert your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Wow, this is MASHA!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
